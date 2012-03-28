984d96f49e63f946e97c11b4e57292f13928c8d8
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2012-03-28 13:18:56 +0300

    static javascript file added and changed to the template done

--- geoforms/views.py
+++ geoforms/views.py
@@ -14,13 +14,3 @@ def questionnaire(request, questionnaire_slug):
     return render_to_response('questionnaire.html',
                              {'form_list': form_list},
                              context_instance = RequestContext(request))
-                             
-class QuestionnaireWizard(SessionWizardView):
-    template_name= 'form_template.html'
-    def done(self, form_list, **kwargs):
-        return render_to_response('done.html', {
-            'form_data': [form.cleaned_data for form in form_list],
-        })
-        
-    def get_form_kwargs(self, step):
-        return {'definition': definitions[int(step)]}
