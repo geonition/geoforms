e3a0cb5179c50ba78b28958a8033786cbc3dbb6a
Kristoffer Snabb <ksnabb@ubuntu-virtual.(none)>
2012-04-26 12:13:40 +0300

    baseline version 4.0.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='geoforms',
-    version='1.0.0',
+    version='4.0.0',
     author='Kristoffer Snabb',
     url='https://github.com/geonition/geoforms',
     packages=find_packages(),
