058fcc759a626a572b774672facb3785741fa5e8
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2012-05-01 20:08:53 +0300

    some fixing and adding for the api, supports textareas now

--- geoforms/templates/questionnaire.api.js
+++ geoforms/templates/questionnaire.api.js
@@ -9,6 +9,130 @@ gnt.questionnaire.popup; //only one popup at the time
 gnt.questionnaire.property_id;
 
 /*
+Draw Button is a drawing
+tool that works together with
+Openlayers.
+
+Required global variables is a
+OpenLayers map object with the
+button required draw controls.
+
+control : The id of the map control to use
+classes : The classes to add to the widget on initialization
+text_class: the class to be added to the span that includes the button text
+active_class: the class to use when a button is activated
+*/
+(function( $ ) {
+    $.widget("ui.drawButton",
+        {
+            options: {
+                drawcontrol: "drawcontrol", //the draw control used, required
+                selectcontrol: "selectcontrol",
+                classes: "ui-button ui-widget ui-state-default ui-corner-all ui-button-text-only",
+                text_class: "ui-button-text",
+                active_class: "ui-state-active",
+                disable_class: "ui-button-disabled ui-state-disabled",
+                rule_added: false,
+                icons: {
+                    primary: undefined,
+                    secondary: undefined
+                }
+            },
+            _create: function() {
+                this.element.addClass( this.options.classes )
+                this.element.bind('click',
+                          this.toggle_active);
+                var label = this.element.html();
+                $( "<span></span>")
+                        .addClass( this.options.text_class )
+                        .appendTo( this.element.empty() )
+                        .html( label )
+                        .text();
+                
+                //add rule to layer for prefered rendering      
+                var color = $(this.element).data('color');
+                var name = $(this.element).attr('name');
+                if(!this.options.rule_added) {
+                    var rule = new OpenLayers.Rule({
+                        filter: new OpenLayers.Filter.Comparison({
+                            type: OpenLayers.Filter.Comparison.EQUAL_TO,
+                            property: 'name',
+                            value: name
+                        }),
+                        symbolizer: {
+                            externalGraphic: '/images/needle?color=' + color,
+                            graphicHeight: 36,
+                            graphicWidth: 23,
+                            graphicYOffset: -30,
+                            cursor: 'pointer'
+                            }
+                        });
+                    var drawcontrol_id = this.options['drawcontrol'];
+                    var drawcontrol = map.getControl(drawcontrol_id);
+                    drawcontrol.layer.styleMap.styles.default.addRules([rule]);
+                    this.options.rule_added = true;
+                }
+            
+                return this;
+            },
+
+            toggle_active: function(evt) {
+                var active_cls = $(this).drawButton('option', 'active_class');
+                if($(this).hasClass( active_cls )) {
+                    $(this).drawButton('deactivate');
+                } else {
+                    $(this).drawButton('activate');
+                }
+            },
+            deactivate: function() {
+                this.element.removeClass( this.options['active_class'] );
+                var drawcontrol_id = this.options['drawcontrol'];
+                var drawcontrol = map.getControl(drawcontrol_id);
+                drawcontrol.deactivate();
+                var selectcontrol_id = this.options['selectcontrol'];
+                var selectcontrol = map.getControl(selectcontrol_id);
+                selectcontrol.activate();
+            },
+            activate: function() {
+                if(this.element.attr( 'disabled') !== 'disabled') {
+                    
+                    //unselect the others
+                    $(".drawbutton." + this.options['active_class'])
+                        .drawButton( 'deactivate' );
+                    this.element.addClass( this.options['active_class'] );
+                    var drawcontrol_id = this.options['drawcontrol'];
+                    var drawcontrol = map.getControl(drawcontrol_id);
+                    drawcontrol.activate();
+                    var selectcontrol_id = this.options['selectcontrol'];
+                    var selectcontrol = map.getControl( selectcontrol_id );
+                    selectcontrol.deactivate();
+                    
+                    //change the temporary style of the layer
+                    var color = $(this.element).data('color');
+                    var name = $(this.element).attr('name');
+                    drawcontrol.layer.styleMap.styles.temporary.defaultStyle.externalGraphic = '/images/needle?color=' + color;
+                }
+            },
+            disable: function() {
+                this.element.removeClass( this.options['active_class'] );
+                this.element.addClass( this.options['disable_class'] );
+                this.element.attr( 'disabled', 'disabled');
+                var drawcontrol_id = this.options['drawcontrol'];
+                var drawcontrol = map.getControl(drawcontrol_id);
+                drawcontrol.deactivate();
+                var selectcontrol_id = this.options['selectcontrol'];
+                var selectcontrol = map.getControl(selectcontrol_id);
+                selectcontrol.activate();
+            },
+            enable: function() {
+                this.element.removeClass( this.options['disable_class'] );
+                this.element.removeAttr( 'disabled' );
+            }
+        });
+})( jQuery );
+
+
+/*
 This is a helper function that returns
 a OpenLayers LonLat object according
 to the geometry that is given to it.
@@ -260,13 +384,14 @@ gnt.questionnaire.on_feature_unselect_handler = function(evt) {
  questionnaire_area -- geojson geometry polygon describint the area of the
                        questionnaire
  data_group -- the group that should be used and where the data is stored
- 
+ callback -- function to be called after the questionnaire has been set up
 */
 gnt.questionnaire.init = function(forms,
                                   popups,
                                   accordion,
                                   questionnaire_area,
-                                  data_group) {
+                                  data_group,
+                                  callback) {
     
     //create a session for the anonymoususer
     gnt.auth.create_session();
@@ -313,13 +438,15 @@ gnt.questionnaire.init = function(forms,
                            {'success': function(data) {
                             
                                 gnt.questionnaire.property_id = data.id;
-                                $(forms + ' input').each(function(i) {
+                                $(forms + ' :input:not(button)').each(function(i) {
                                     
                                     if(data[this.name] !== undefined) {
                                         if(this.type === 'radio') {
                                             if(this.value === data[this.name]) {
                                                 $(this).attr('checked', true);
                                             }
+                                        } else if(this.type === 'textarea') {
+                                            $(this).text(data[this.name]);
                                         } else {
                                             this.value = data[this.name];
                                         }
@@ -328,7 +455,7 @@ gnt.questionnaire.init = function(forms,
                            },
                            'complete': function() {
                                 //bind on value change to save the values
-                                $(forms + ' input').change(function(evt) {
+                                $(forms + ' :input:not(button)').change(function(evt) {
                                     var property = {};
                                     property[evt.currentTarget.name] = evt.currentTarget.value;
                                     if(gnt.questionnaire.property_id === undefined) {
@@ -490,5 +617,10 @@ gnt.questionnaire.init = function(forms,
                    }
                }
            });
+        
+        //the point where everything is done for callback
+        if(callback !== undefined) {
+            callback();
+        }
     });
 };
