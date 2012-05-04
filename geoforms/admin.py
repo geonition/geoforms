e499a2ca6a702035422b4a51604e7a703eed7daf
ubuntu <ubuntu@ubuntu-VirtualBox.(none)>
2012-05-04 11:48:00 +0300

    Improved map for admin

--- geoforms/admin.py
+++ geoforms/admin.py
@@ -4,16 +4,28 @@ from models import GeoformElement
 from models import FormElement
 from models import Questionnaire
 from models import QuestionnaireForm
+from django.conf import settings
 
 class GeoformElementAdmin(admin.ModelAdmin):
     ordering = ['name']
 
 class FormElementAdmin(admin.ModelAdmin):
     ordering = ['geoform', 'order']
+
+class QuestionnaireAdmin(admin.OSMGeoAdmin):
+    default_lon = getattr(settings,
+                          'ORGANIZATION_ADMIN_DEFAULT_MAP_SETTINGS',
+                          {'default_lon': 0})['default_lon']
+    default_lat = getattr(settings,
+                          'ORGANIZATION_ADMIN_DEFAULT_MAP_SETTINGS',
+                          {'default_lat': 0})['default_lat']
+    default_zoom = getattr(settings,
+                          'ORGANIZATION_ADMIN_DEFAULT_MAP_SETTINGS',
+                          {'default_zoom': 4})['default_zoom'] 
     
 admin.site.register(GeoformElement, GeoformElementAdmin)
 admin.site.register(Geoform)
 admin.site.register(FormElement, FormElementAdmin)
-admin.site.register(Questionnaire, admin.OSMGeoAdmin)
+admin.site.register(Questionnaire, QuestionnaireAdmin)
 admin.site.register(QuestionnaireForm)
 
