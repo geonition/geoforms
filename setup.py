265f9766621f393d5bbc6cf92012a44aaa1d692e
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2012-05-30 12:24:57 +0300

    version changed in setup.py script

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='geoforms',
-    version='4.1.0',
+    version='4.1.2',
     author='Kristoffer Snabb',
     url='https://github.com/geonition/geoforms',
     packages=find_packages(),
