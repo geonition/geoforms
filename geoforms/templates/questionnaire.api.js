cf450fce48b56396641d785a11e7549687b32e95
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2012-07-13 14:53:33 +0300

    tooltip changes so it works better together with other apps

--- geoforms/templates/questionnaire.api.js
+++ geoforms/templates/questionnaire.api.js
@@ -1,3 +1,5 @@
+{% include "jstranslations.txt" %}
+
 /*
  Questionnaire UI namespace
  
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='geoforms',
-    version='4.1.7',
+    version='4.1.8',
     author='Kristoffer Snabb',
     url='https://github.com/geonition/geoforms',
     packages=find_packages(),
