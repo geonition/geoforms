1f30f3b35cb8fb7f06ad876d71b0144be9f728f1
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2012-08-16 11:15:06 +0300

    the popups are now added auomatically, this is not efficient from performance point fo view but let's live with that for now

--- geoforms/views.py
+++ geoforms/views.py
@@ -1,4 +1,6 @@
+from bs4 import BeautifulSoup
 from django.shortcuts import render_to_response
+from geoforms.models import Geoform
 from geoforms.models import Questionnaire
 from django.template import RequestContext
 from django.views.decorators.csrf import ensure_csrf_cookie
@@ -12,10 +14,16 @@ def questionnaire(request, questionnaire_slug):
     quest = Questionnaire.on_site.select_related().get(slug = questionnaire_slug)
     form_list = quest.geoforms.all().order_by('questionnaireform__order')
     elements = {}
+    popup_set = set()
     for form in form_list:
+        popup_elements = form.elements.filter(element_type = 'drawbutton').values_list('html', flat=True)
+        for e in popup_elements:
+            soup = BeautifulSoup(e)
+            popup_set.add(soup.button['data-popup'])
+            
         elements[form.slug] = form.elements.order_by('formelement__order', '?')
-    
-    popup_list = form_list.filter(type = 'popup')
+        
+    popup_list = Geoform.objects.filter(type = 'popup').filter(name__in = popup_set)
     form_list = form_list.filter(type = 'form')
     
     return render_to_response('questionnaire.html',
