3adcc7beeb5b366449d68beefca7da01ee3972a4
Mikko Johansson <mikko.johansson@aalto.fi>
2012-09-14 13:59:50 +0300

    increased version number 5.3.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='geoforms',
-    version='5.3.0',
+    version='5.3.1',
     author='Kristoffer Snabb',
     url='https://github.com/geonition/geoforms',
     packages=find_packages(),
