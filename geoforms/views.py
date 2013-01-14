d8cb7742bfa3ff68337b01909b5fa01d5c2c3174
Mikko Johansson <mikko.johansson@aalto.fi>
2013-01-14 17:23:05 +0200

    added fallback of the coordinate system if maps application is not installed

--- geoforms/views.py
+++ geoforms/views.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 from django.shortcuts import render_to_response
+from django.core.exceptions import ObjectDoesNotExist
 from geoforms.models import Geoform
 from geoforms.models import Questionnaire
 from django.template import RequestContext
@@ -7,7 +8,7 @@ from django.views.decorators.csrf import ensure_csrf_cookie
 
 from django.conf import settings
 # we need srid of the questionnaire map
-from maps.models import Map
+#from maps.models import Map
 
 
 @ensure_csrf_cookie
@@ -15,14 +16,32 @@ def questionnaire(request, questionnaire_slug):
     """
     This view creates the whole questionnaire html.
     """
+    # Check if we have maps application installed, if not use geometry column srid
+    try:
+        from maps.models import Map
+        # At the moment questionnaires always use map named questionnaire map.
+        # This should not be hardcoded.
+        # If named map is not found we use geometry column srid.
+        map_srid = int(Map.objects.get(slug_name = 'questionnaire-map').projection)
+        #take initial all transformed to map coordinates
+    except (ImportError, ObjectDoesNotExist):
+        #take initial all
+        map_srid = None
+        
     # At the moment questionnaires always use map named questionnaire map.
     # This should not be hardcoded
-    map_srid = int(Map.objects.get(slug_name = 'questionnaire-map').projection)
+    #map_srid = int(Map.objects.get(slug_name = 'questionnaire-map').projection)
     
 #    quest = Questionnaire.on_site.select_related().get(slug = questionnaire_slug)
-    quest = Questionnaire.objects.filter(
-                      site__id__exact = settings.SITE_ID).transform(
-                      map_srid).select_related().get(slug = questionnaire_slug)
+    if map_srid is not None:
+        quest = Questionnaire.objects.filter(
+                          site__id__exact = settings.SITE_ID).transform(
+                          map_srid).select_related().get(slug = questionnaire_slug)
+    else:
+        quest = Questionnaire.objects.filter(
+                          site__id__exact = settings.SITE_ID).select_related().get(
+                          slug = questionnaire_slug)
+        
     form_list = quest.geoforms.all().order_by('questionnaireform__order')
     elements = {}
     popup_set = set(Geoform.objects.filter(page_type = 'popup').values_list('slug', flat=True))
