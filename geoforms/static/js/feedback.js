a5569bd213d139f77157013746c3df56dd3d1a38
Sakari Ellonen <sakari.ellonen@gmail.com>
2013-05-23 18:24:25 +0300

    typo

--- geoforms/static/js/feedback.js
+++ geoforms/static/js/feedback.js
@@ -148,7 +148,6 @@ function parse_features(data){
                 }
                 for (var a_value in feature.attributes.form_values[a_name]){
                     if (a_value.indexOf(' ') != -1 || !(isNaN(Number(a_value)))){
-                        console.log('open question: ' + a_value);
                         continue;
                     }
                     var a_v_key = simple_hash([f_name, a_name, a_value].join('-AND-'));
