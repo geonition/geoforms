31417e641ca75ede1e242e00fe11fdc21ce595de
Sakari Ellonen <sakari.ellonen@gmail.com>
2013-05-23 13:38:44 +0300

    changed feature coloring to d2 ordinal colors

--- geoforms/static/js/feedback.js
+++ geoforms/static/js/feedback.js
@@ -4,15 +4,9 @@ vizu_features = {};
 function make_style_getter(){
     var current = 0;
     var name2color = {};
-    var colorlist = ['#CC0000',
-                    '#990099',
-                    '#3333CC',
-                    '#0099FF',
-                    '#29A3A3',
-                    '#297A29',
-                    '#33D633',
-                    '#E6E600',
-                    '#663300'];
+    var colorlist = [
+'#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf', '#9edae5', '#98df8a', '#aec7e8', '#ffbb78', '#ff9896', '#c5b0d5', '#c49c94', '#f7b6d2', '#c7c7c7', '#dbdb8d'
+        ];
     return function(name){
         if (!(name in name2color))
         {
